e94d6b7aa7701931860d6e028bb7ed35a4e2d4f7
SequeI <asiek@redhat.com>
2025-11-13 09:27:01 +0000

    chore: bump images
    Signed-off-by: SequeI <asiek@redhat.com>

--- internal/constants/images.go
+++ internal/constants/images.go
@@ -24,5 +24,5 @@ var (
 	// ModelTransparencyCliImage is the default image for the model transparency CLI
 	// used as an init container to validate model signatures
 	ModelTransparencyCliImage = "registry.redhat.io/rhtas/model-transparency-rhel9@" +
-		"sha256:a03de6850e5a9a948541761de13d903ffb35d78cad4433d1cf67fbdbfed41891"
+		"sha256:c9704230cc525fe00b16aced00d31dc56c6105c630849d0a72c558c0aa4c2c60"
 )
