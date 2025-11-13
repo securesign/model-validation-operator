070be60f96b16d1ac14de07c883b9d20b8c14fed
Aleks <121458075+SequeI@users.noreply.github.com>
2025-11-13 13:44:29 +0000

    fix: bundle generation (#36)
    Signed-off-by: SequeI <asiek@redhat.com>

---
+++ hack/build-bundle.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+KUSTOMIZATION_FILE="config/manager/kustomization.yaml"
+
+IMG_NAME="${IMG%@*}"
+IMG_DIGEST="${IMG#*@}"
+
+cat << EOF >> "${KUSTOMIZATION_FILE}"
+
+images:
+- digest: ${IMG_DIGEST}
+  name: controller
+  newName: ${IMG_NAME}
+EOF
+
+# Generate and validate the Operator bundle
+oc kustomize config/manifests | operator-sdk generate bundle ${BUNDLE_GEN_FLAGS}
+
+CSV="bundle/manifests/model-validation-operator.clusterserviceversion.yaml"
+
+if [[ -f "${CSV}" ]]; then
+  sed -i.bak  's/deploymentName: webhook/deploymentName: model-validation-controller-manager/' "${CSV}"
+  sed -i.bak2 's/deploymentName: model-validation-controller-manager/deploymentName: model-validation-controller-manager\
+    serviceName: model-validation-webhook\
+    containerPort: 9443/' "${CSV}"
+  rm -f "${CSV}.bak" "${CSV}.bak2"
+fi
+
+operator-sdk bundle validate ./bundle
